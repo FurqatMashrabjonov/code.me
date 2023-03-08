8c8810421f816beaaff92bc1ac2dbc2ec03e2ca5
FurqatMashrabjonov <php_lesson@mail.ru>
2023-03-08 20:54:58 +0500

    jetstream

---
+++ app/Http/Controllers/ProblemTagController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProblemTag;
+use Illuminate\Http\Request;
+
+class ProblemTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProblemTag $problemTag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProblemTag $problemTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProblemTag $problemTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProblemTag $problemTag)
+    {
+        //
+    }
+}
